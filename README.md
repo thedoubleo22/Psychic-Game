d84b431e8ad61abd0b82748b9967451bce91aeaf
Olivia Oxley <oliviaoxley@olivias-macbook.attlocal.net>
2018-10-08 23:17:16 -0700

    finishing touches

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Psychic-Game
 Week 3 HW
+
+Instructions!!
+
+The purpose of this game is for the user to guess what alphabetic letter the computer is thinking/generating. The user has 10 chances to guess and then the game will end(reset). If the users choice === computer choice one point will be generated to wins. If the user choice != computer choice one point will be generated to loss. 
+
+<b> I. Variables </b>
+
+1.Letters
+2.Wins Score
+3.Loss Score
+4.Guesses Lefts
+5. Guesses So Far
+6. Computer Choice
+7. User Choice
+
+<b> II. Functions </b>
+1. Linking the HTML to Javascript through document.queryselector.
+2. On key functions.
+3. Math operators to generate a random letter (computer choice).
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,8 +10,56 @@ Game Instructions
 */
 
 //Chapter 1 Variable Initialization 
+var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"]
+var winScore = 0
+var lossScore = 0
+var guessLeft = 10
+var letterIndex;
+var letterGenerator;
 
-var letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var wins = 0
-var loss = 0
-var guessLeft = 9
+
+//Chapter 2 Functions
+function winFunction() {
+	return document.getElementById("wins").innerHTML = winScore
+}
+
+function loseFunction() {
+	return document.getElementById("lose").innerHTML = lossScore
+}
+
+function guessLeftFunction() {
+  return document.getElementById("left").innerHTML = guessLeft
+}
+
+function reset() {
+	document.getElementById("guess").innerHTML = ""
+	guessLeft = 10;
+	guessLeftFunction()
+    letterIndex = Math.floor(Math.random() * letter.length);
+    letterGenerator = letter[letterIndex]
+}
+
+winFunction()
+loseFunction()
+reset()
+
+//Chapter 3 User Interactions - Events
+document.onkeyup = function(event) {
+    var userChoice = event.key.toLowerCase();
+
+    if (userChoice === letterGenerator) {
+        winScore++
+        winFunction()
+        reset()
+    } else {
+        guessLeft--
+        guessLeftFunction()
+        document.getElementById("guess").innerHTML += (userChoice + ", ")
+
+        if (guessLeft === 0) {
+        	lossScore++
+        	loseFunction()
+        	reset()
+        }
+    }
+}
